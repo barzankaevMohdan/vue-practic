f2b408025799c6a8e3aff89da432bde49d2d2ed0
Mohdan <mohdan.barzankaev@gmail.com>
2021-05-22 22:11:45 +0300

    validation and retrieval of values

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,65 @@
 <template>
   <div class="container">
-    <form class="card">
+    <form class="card" @submit.prevent="submitHandler">
       <h1>Анкета на Vue разработчика!</h1>
       <div class="form-control">
         <label for="name">Как тебя зовут?</label>
-        <input type="text" id="name" placeholder="Введи имя">
+        <input 
+        type="text" 
+        id="name" 
+        placeholder="Введи имя"
+        v-model.trim="name"
+        >
       </div>
 
       <div class="form-control">
         <label for="age">Выбери возраст</label>
-        <input type="number" id="age" value="20">
+        <input 
+        type="number" 
+        id="age" 
+        max="70"
+        v-model.number="age"
+        >
       </div>
 
       <div class="form-control">
         <label for="city">Твой город</label>
-        <select id="city">
+        <select id="city" v-model="city">
           <option value="spb">Санкт-Петербург</option>
           <option value="msk">Москва</option>
           <option value="kzn">Казань</option>
-          <option selected value="nsk">Новосибирск</option>
+          <option value="nsk">Новосибирск</option>
         </select>
       </div>
 
       <div class="form-checkbox">
         <span class="label">Готов к переезду в Токио?</span>
         <div class="checkbox">
-          <label><input type="radio" name="trip"/> Да</label>
+          <label><input type="radio" v-model="relocate" name="trip" value="yes"/> Да</label>
         </div>
 
         <div class="checkbox">
-          <label><input type="radio" name="trip"/> Нет</label>
+          <label><input type="radio" v-model="relocate" name="trip" value="no"/> Нет</label>
         </div>
       </div>
 
       <div class="form-checkbox">
         <span class="label">Что знаешь во Vue?</span>
         <div class="checkbox">
-          <label><input type="checkbox"/> Vuex</label>
+          <label><input type="checkbox" v-model="skills" name="skills" value="vuex"/> Vuex</label>
         </div>
         <div class="checkbox">
-          <label><input type="checkbox"/> Vue CLI</label>
+          <label><input type="checkbox" v-model="skills" name="skills" value="cli"/> Vue CLI</label>
         </div>
         <div class="checkbox">
-          <label><input type="checkbox"/> Vue Router</label>
+          <label><input type="checkbox" v-model="skills" name="skills" value="router"/> Vue Router</label>
+        </div>
+      </div>
+
+      <div class="form-checkbox">
+      <span class="label">Правила сервиса</span>
+        <div class="checkbox">
+          <label><input type="checkbox" v-model="agree"/> Согласен с условиями</label>
         </div>
       </div>
 
@@ -52,7 +69,30 @@
 </template>
 
 <script>
-  export default {}
+  export default {
+    data() {
+      return {
+        name: '',
+        age: 23,
+        city: 'spb',
+        relocate: null,
+        skills: [],
+        agree: false
+      }
+    },
+    methods: {
+      submitHandler() {
+        console.group('form data')
+        console.log('Name', this.name)
+        console.log('Age', this.age)
+        console.log('City', this.city)
+        console.log('To Tokyo', this.relocate)
+        console.log('Skills', this.skills)
+        console.log('Agree', this.agree)
+        console.groupEnd()
+      }
+    }
+  }
 </script>
 
 <style>
